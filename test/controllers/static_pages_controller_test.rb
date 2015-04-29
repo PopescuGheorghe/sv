2845367764229f874484f195f5b443ebaab77530
PopescuGheorghe <gimmy.xdd@gmail.com>
2015-04-29 11:59:51 +0300

    Added contact page and named routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -19,4 +19,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "About | Ruby on rails application"
   end
 
+  test "should get contact"  do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Ruby on rails application"
+  end
+
 end
