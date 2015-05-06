d1989b8a79d537efd3824a2394f9dcfb6cf76543
PopescuGheorghe <gimmy.xdd@gmail.com>
2015-05-06 20:04:51 +0300

    added mail attachement

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,11 @@ class UserMailer < ApplicationMailer
 	def welcome_email(user)
 		@user = user
 		@url = 'http://exmaple.com'
+		file = File.join(Rails.root,'tmp','images.jpg')
+		attachments['images.jpg'] = File.read(file)
 		mail(to: user,
 			subject: "Hello ! This is an automated mail")
+		
 
 	end
 end
